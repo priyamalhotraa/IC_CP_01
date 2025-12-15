50ce45a4b2f10224428734536cbcd8cdab4288d9
priyamalhotraa <priyamalhotra14012005@gmail.com>
2025-12-16 00:58:37 +0530

    Add C++ number guessing game solution
    This commit adds the C++ implementation of the Number Guessing Game
    for Task 1 assigned by InternCred.

---
+++ numberguess.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    int difficulty;
+    int maxAttempts;
+    int userGuess;
+    int attemptsUsed=0;
+    int previousGuesses[10]; //array for storing previous guesses
+
+    cout<<"Select Difficulty Level\n";
+    cout<<"1.Easy (10 attempts)\n";
+    cout<<"2.Medium (7 attempts)\n";
+    cout<<"3.Hard (5 attempts)\n";
+    cout<<"Choose Difficulty Level (1,2 or 3): ";
+    cin>>difficulty;
+
+    if(difficulty==1){
+        maxAttempts=10;
+    }
+    else if(difficulty==2){
+        maxAttempts=7;
+    }
+    else if(difficulty==3){
+        maxAttempts=5;
+    }
+    srand(time(0));
+    int randomnumber=(rand()%(100-1)+1);
+
+    cout<<"\nGuess the number between 1 and 100\n";
+
+    while(attemptsUsed<maxAttempts){
+        cout<<"Enter your guess: ";
+        cin>>userGuess;
+
+        previousGuesses[attemptsUsed]=userGuess;
+        attemptsUsed++;
+
+        if(userGuess>randomnumber) cout<<"Too High!";
+        else if(userGuess<randomnumber) cout<<"Too Low!";
+        else{
+            cout<<"Congratulations!! You guessed it correctly.\n";
+            cout<<"Attempts Used: "<<attemptsUsed;
+            return 0;
+        }
+        cout<<"\nGuesses left: "<<maxAttempts-attemptsUsed<<endl;
+    }
+    cout<<"\n Game Over!";
+    cout<<"\nThe correct number was: "<<randomnumber;
+    return 0;
+}
+    
